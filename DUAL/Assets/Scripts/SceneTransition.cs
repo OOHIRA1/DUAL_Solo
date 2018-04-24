78df61dd4900fa626640bfd9746f50f0b8194183
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-24 21:28:04 +0900

    ステージセレクトシーンの作成、シーン遷移機能の追加
    ・ステージセレクトシーンを作成しました
    ・シーン遷移機能を追加しました
    ・Titleシーン上をタップするとStageSelectシーンに遷移するようにしました

---
+++ DUAL/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//==シーン遷移機能を付けるクラス
+//
+//使い方：シーン遷移機能を付けたいGameObjectにアタッチ
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//-----------------------------------------------------------
+	//public関数
+	//-----------------------------------------------------------
+
+	//--シーン遷移をする関数
+	public void SceneChange( string nextScene ) {
+		SceneManager.LoadScene ( nextScene );
+	}
+	//-----------------------------------------------------------
+	//-----------------------------------------------------------
+}
