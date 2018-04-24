36c8f5ba8bf2332ee34cf241cf3e563c2c1d930a
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-25 07:23:29 +0900

    背景のアニメーション追加
    ・背景のアニメーションを追加しました
    ・背景アニメーション後にシーン遷移するようにしました

---
+++ DUAL/Assets/Scripts/BackgroundAnimationControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==背景のアニメーションを管理するクラス
+//
+//使用方法：アニメーションをする背景にアタッチ
+public class BackgroundAnimationControll : MonoBehaviour {
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//---------------------------------------------------------------
+	//public関数
+	//---------------------------------------------------------------
+
+	//--背景画像を取り外すアニメーションをさせる関数
+	public void Remove( ) {
+		_animator.SetTrigger ( "removeTrigger" );
+	}
+
+
+	//--背景画像が取り外されたフラグ(removedFlag)を立てる関数
+	public void SetRemovedFlag( ) {
+		_animator.SetBool ( "removedFlag", true );
+	}
+
+
+	//--背景画像が取り外されたフラグ(removedFlag)の状況を返す関数
+	public bool GetRemovedFlag( ) {
+		return _animator.GetBool ( "removedFlag" );
+	}
+	//---------------------------------------------------------------
+	//---------------------------------------------------------------
+}
---
+++ DUAL/Assets/Scripts/TitleSceneControll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==Titleシーンを管理するクラス
+//
+//使用方法：常にアクティブなゲームオブジェクトにアタッチ
+[RequireComponent( typeof(SceneTransition) )]
+public class TitleSceneControll : MonoBehaviour {
+	SceneTransition _sceneTransition;
+	[SerializeField]BackgroundAnimationControll _backgrondAnimationControll = null;
+
+	// Use this for initialization
+	void Start () {
+		_sceneTransition = GetComponent<SceneTransition> ();
+		if (!_backgrondAnimationControll) {
+			Debug.LogError ( "_backgrondAnimationControllを設定してください" );
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_backgrondAnimationControll.GetRemovedFlag ()) {
+			_sceneTransition.SceneChange ( "StageSelect" );
+		}
+	}
+}
