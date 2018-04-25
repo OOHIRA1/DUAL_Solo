7e5a3aa2234c39dc23908823e05ad8dfe1c72b2e
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-25 18:58:39 +0900

    ボタンアニメーション作成（途中）
    ・ステージボタンを押すと中心に半回転しながら移動するようにしました

---
+++ DUAL/Assets/Scripts/StageButtonAnimationControll.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//==StageButtonのアニメーションを管理するクラス
+//
+//使用方法：全てのボタンの親オブジェクトにアタッチ
+public class StageButtonAnimationControll : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//--------------------------------------------------------
+	//public関数
+	//---------------------------------------------------------
+
+	//--ボタンをアップする関数
+	public void Up( GameObject x ) {
+		//RectTransform rectTransform = x.gameObject.GetComponent<RectTransform>();
+		//rectTransform.anchoredPosition = new Vector2 ( 0, 0 );
+		StartCoroutine( "UpCoroutin", x );
+	}
+
+	public IEnumerator UpCoroutin( GameObject button ) {
+		const int ANIMATIONFLAME = 90;
+		RectTransform rectTransform = button.GetComponent<RectTransform>();
+		Vector2 vec = -rectTransform.anchoredPosition / ( ANIMATIONFLAME - 30 );
+		Vector3 rotation = new Vector3( 0, 180 / (ANIMATIONFLAME - 30), 0);
+		for (int i = 0; i < ANIMATIONFLAME; i++) {
+			if (i < ANIMATIONFLAME - 30) {
+				rectTransform.anchoredPosition += vec;
+				rectTransform.localEulerAngles += rotation;
+			} else if (i == ANIMATIONFLAME - 30) {
+				rectTransform.anchoredPosition = Vector2.zero;
+				rectTransform.localEulerAngles = new Vector3 ( 0, 180, 0 );
+			}
+			yield return null;
+		}
+	}
+	//--------------------------------------------------------
+	//----------------------------------------------------------
+}
---
+++ DUAL/Assets/Scripts/StageSelectSceneControll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==StageSelectシーンを管理するクラス
+//
+//使用方法：常にアクティブなゲームオブジェクトにアタッチ
+public class StageSelectSceneControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
